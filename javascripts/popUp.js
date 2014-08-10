6b59bd7ee1acca6bc860672d18f5c0975a1fec2d
afoysal <nowphp@gmail.com>
2014-08-10 13:03:21 +0600

    Update popUp.js

--- javascripts/popUp.js
+++ javascripts/popUp.js
@@ -73,8 +73,9 @@
 					width: params.after_pop_up_div_width
 				  },500,function()
 				  {
-					$('<img id="pupup_cancle" src="https://cloud.githubusercontent.com/assets/1283998/3868676/ab55544a-205a-11e4-83ff-b1008f28d277.gif" height="16" width="16"/>')  
+					$('<img id="pupup_cancle" src="https://cloud.githubusercontent.com/assets/1283998/3868676/ab55544a-205a-11e4-83ff-b1008f28d277.gif" height="16" width="16" align="right"/>')  
 				  	.appendTo('#popup')
+				  	.css({zIndex : 10}) 
 				  	.hide()
 					.fadeIn(1000);
 				  });
