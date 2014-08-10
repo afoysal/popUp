8f15187b8e26d36c35c8e7524ff6fda014bfecca
afoysal <nowphp@gmail.com>
2014-08-10 15:18:49 +0600

    Update popUp.js

--- javascripts/popUp.js
+++ javascripts/popUp.js
@@ -76,8 +76,7 @@
 					$('<img id="pupup_cancle" src="https://cloud.githubusercontent.com/assets/1283998/3868676/ab55544a-205a-11e4-83ff-b1008f28d277.gif" height="16" width="16" align="right"/>')  
 				  	.appendTo('#popup')
 				  	.css({
-				  		border: 0,
-				  		box-shadow: #ffffff 
+				  		border: 0
 				  	})
 				  	.hide()
 					.fadeIn(1000);
